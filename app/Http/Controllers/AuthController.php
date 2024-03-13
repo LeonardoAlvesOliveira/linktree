7df95742fe7c680f885162aecaf0cc894f16c3da
Leonardo <126174633+LeonardoAlvesOliveira@users.noreply.github.com>
2024-03-13 12:17:31 -0300

    f

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use Psy\CodeCleaner\ReturnTypePass;
 
 class AuthController extends Controller
@@ -12,5 +16,54 @@ class AuthController extends Controller
         return response()->json([
             'error' => 'Não autorizado'
         ], 401);
+    }
+    public function register(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'cpf' => 'required|digits:11|unique:users,cpf',
+            'password' => 'required',
+            'password_confirm' => 'required|same:password'
+        ]);
+
+        if (!$validator->fails()) {
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $cpf = $request->input('cpf');
+            $password = $request->input('password');
+
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            $newUser = new User();
+            $newUser->name = $name;
+            $newUser->email = $email;
+            $newUser->password = $hash;
+            $newUser->cpf = $cpf;
+            $newUser->save();
+
+            $token = auth()->attempt([
+                'cpf' => $cpf,
+                'password' => $password
+            ]);
+            if ($token) {
+                $array['error'] = 'Ocorreu um erro';
+                return $array;
+            }
+            $array['token'] = $token;
+            $user = auth()->user();
+            $array['user'] = $user;
+
+            $proprieties = Unit::select(['id', 'name'])->where('id_owner', $user['id'])->get();
+
+            $array['user']['proprieties'] = $proprieties;
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
     }
 }
